d216cd093843017ea8742c38d2a29549a9b038f6
phecho <phecho@phecho.com>
2016-06-15 20:54:00 +0800

    again

---
+++ app/Handlers/Events/Link/RemoveLinkCacheHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of Hifone.
+ *
+ * (c) Hifone.com <hifone@hifone.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Hifone\Handlers\Events\Link;
+
+use Cache;
+use Hifone\Events\EventInterface;
+
+class RemoveLinkCacheHandler
+{
+    public function handle(EventInterface $event)
+    {
+        Cache::forget('links');
+        \Log::info('forget cache: links');
+    }
+}
