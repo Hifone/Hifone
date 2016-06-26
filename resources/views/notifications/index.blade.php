f2521c5104d5b9327e269d69dcdb9d9c6834742f
phecho <phecho@163.com>
2016-06-26 21:44:52 +0800

    Add notification partial templates

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -16,69 +16,22 @@
     </div>
 
     @if (count($notifications))
+	<div class="panel-body remove-padding-horizontal notification-index">
 
-        <div class="panel-body remove-padding-horizontal notification-index">
-
-            <ul class="list-group row">
-                @foreach ($notifications as $notification)
-                 <li class="list-group-item media" style="margin-top: 0px;">
-
-                    @if (count($notification->user))
-                        <div class="avatar pull-left">
-                            <a href="{!! route('user.show', [$notification->from_user_id]) !!}">
-                                <img class="media-object img-thumbnail avatar" alt="{!! $notification->author->username !!}" src="{!! $notification->author->avatar_small !!}"  style="width:38px;height:38px;"/>
-                            </a>
-                        </div>
-
-                        <div class="infos">
-
-                          <div class="media-heading">
-
-                            <a href="{!! route('user.show', [$notification->from_user_id]) !!}">
-                                {!! $notification->author->username !!}
-                            </a>
-                             •
-                            {!! $notification->labelUp !!}
-                            @if(!in_array($notification->type,['user_follow','credit_register','credit_login', 'thread_new_reply','reply_mention']))
-                            <a href="{!! route('thread.show', [$notification->object->id]) !!}{!! !empty($notification->reply_id) ? '#reply' . $notification->reply_id : '' !!}" title="{!! $notification->object->title !!}">
-                                {!! str_limit($notification->object->title, '100') !!}
-                            </a>
-                            @elseif(in_array($notification->type, ['thread_new_reply','reply_mention']))
-                            <a href="{!! route('thread.show', [$notification->object->thread->id]) !!}{!! !empty($notification->object->id) ? '#reply' . $notification->object_id : '' !!}" title="{!! $notification->object->thread->title !!}">
-                            {!! str_limit($notification->object->thread->title, '100') !!}
-                            </a>
-                            @endif
-                            <span class="meta">
-                                 • {!! trans('hifone.at') !!} • <span class="timeago">{!! $notification->created_at !!}</span>
-                            </span>
-                          </div>
-                          <div class="media-body markdown-reply content-body">
-                            @if(in_array($notification->type, ['credit_register', 'credit_login']))
-                            {{ trans('hifone.credits.credits') }}: {{ $notification->object->rule->reward }}
-                            @endif
-                            {!! $notification->body !!}
-                          </div>
-
-                        </div>
-                    @else
-                      <div class="deleted text-center">{!! trans('hifone.notifications.deleted') !!}</div>
-                    @endif
-                </li>
-                @endforeach
-            </ul>
-
-
-        </div>
-
-        <div class="panel-footer text-right remove-padding-horizontal pager-footer">
-            <!-- Pager -->
-            {!! $notifications->render() !!}
-        </div>
-
+		<ul class="list-group row">
+			@foreach ($notifications as $notification)
+				@include('notifications.partials.' . $notification->template)
+			@endforeach
+		</ul>
+	</div>
+	<div class="panel-footer text-right remove-padding-horizontal pager-footer">
+		<!-- Pager -->
+		{!! $notifications->render() !!}
+	</div>
     @else
-        <div class="panel-body">
-            <div class="empty-block">{!! trans('hifone.notifications.noitem') !!}</div>
-        </div>
+	<div class="panel-body">
+		<div class="empty-block">{!! trans('hifone.notifications.noitem') !!}</div>
+	</div>
     @endif
 
 </div>
