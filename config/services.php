eaf5690f69cb4945cd3d18fda0d3b73400243e4e
Phecho <phecho@phecho.com>
2016-06-28 15:32:08 +0800

    update

--- config/services.php
+++ config/services.php
@@ -46,6 +46,11 @@ return [
         'client_secret' => env('GITLAB_SECRET'),
         'redirect'      => env('GITLAB_REDIRECT_URI'),
     ],
+    'github' => [
+        'client_id'     => env('GITHUB_KEY'),
+        'client_secret' => env('GITHUB_SECRET'),
+        'redirect'      => env('GITHUB_REDIRECT_URI'),
+    ],
     'google' => [
         'client_id'     => env('GOOGLE_KEY'),
         'client_secret' => env('GOOGLE_SECRET'),
