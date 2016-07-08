6f26381e65845a98c11fa439d75e9c7cff2416d0
Marcel Hering <broxxer@broxxer.de>
2016-07-08 16:43:08 +0200

    German Translation files

---
+++ resources/lang/de/install.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of Hifone.
+ *
+ * (c) Hifone.com <hifone@hifone.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'install'             => 'Installation',
+    'title'               => 'Installations Assistent',
+    'service_details'     => 'Service Details',
+    'env_check'           => 'Umgebung prüfen',
+    'env_setup'           => 'Umgebung einrichten',
+    'config_setup'        => 'Konfiguration setzen',
+    'show_support'        => 'Show support for Hifone?',
+    'admin_account'       => 'Admin Account',
+    'complete_install'    => 'Installation ist fertig.',
+    'completed'           => 'Hifone has been configured successfully!',
+    'finish_install'      => 'Go to dashboard',
+    'next_step'           => '下一步',
+    'prev_step'           => '上一步',
+
+    'email'            => 'Email',
+    'username'         => 'Username',
+    'password'         => 'Password',
+    'site_name'        => 'Site Name',
+    'site_domain'      => 'Site Domain',
+    'site_timezone'    => 'Select your timezone',
+    'site_locale'      => 'Select your language',
+    'enable_google2fa' => 'Enable Google Two Factor Authentication',
+    'cache_driver'     => 'Cache Driver',
+    'session_driver'   => 'Session Driver',
+
+    'role_admin'       => 'Administrator',
+    'role_founder'     => 'Gründer',
+];
