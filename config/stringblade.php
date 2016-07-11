ae8a9f90d64b27fb3e01b3c465bb69b767ef0302
phecho <phecho@163.com>
2016-07-11 12:24:20 -0400

    Applied fixes from StyleCI

--- config/stringblade.php
+++ config/stringblade.php
@@ -1,9 +1,17 @@
 <?php
 
+/*
+ * This file is part of Hifone.
+ *
+ * (c) Hifone.com <hifone@hifone.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-return array(
+return [
 
-	/*
+    /*
     |--------------------------------------------------------------------------
     | Cache
     |--------------------------------------------------------------------------
@@ -23,4 +31,4 @@ return array(
     */
     'cache_time' => 10,
 
-);
+];
