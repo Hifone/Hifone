38473963c9906416729d4e4f723f490cdb0c39d1
Marcel Hering <ryg@banncity.de>
2016-07-22 09:59:52 +0200

    Socialite possibilities

--- config/services.php
+++ config/services.php
@@ -56,4 +56,14 @@ return [
         'client_secret' => env('GOOGLE_SECRET'),
         'redirect'      => env('GOOGLE_REDIRECT_URI'),
     ],
+    'facebook' => [
+        'client_id'     => env('FACEBOOK_KEY'),
+        'client_secret' => env('FACEBOOK_SECRET'),
+        'redirect'      => env('FACEBOOK_REDIRECT_URI'),
+    ],
+    'twitter' => [
+        'client_id'     => env('TWITTER_KEY'),
+        'client_secret' => env('TWITTER_SECRET'),
+        'redirect'      => env('TWITTER_REDIRECT_URI'),
+    ],
 ];
