510d9ff197dafb9684bc133224d2e1d586afdb62
Marcel Hering <ryg@banncity.de>
2016-08-10 02:49:36 +0200

    Fix 152&160 - Fileuploadsize & Underline after Pagenames

--- app/Handlers/Commands/Image/UploadImageCommandHandler.php
+++ app/Handlers/Commands/Image/UploadImageCommandHandler.php
@@ -31,15 +31,40 @@ class UploadImageCommandHandler
         $extension = $file->getClientOriginalExtension() ?: 'png';
         $folderName = '/uploads/images/'.date('Ym', time()).'/'.date('d', time()).'/'.Auth::user()->id;
         $destinationPath = public_path().'/'.$folderName;
-        $safeName = str_random(10).'.'.$extension;
+        // Create Randomstring for Filename
+        $random_string = str_random(10);
+        //Path to Thread Size
+        $safeName = $random_string.'.'.$extension;
+        //Path to Original Size
+        $safeNameOrig = $random_string.'_orig.'.$extension;
+        //Path to LightBox Size
+        $safeNameLightbox = $random_string.'_lightbox.'.$extension;
+        // Copy the File for Preserving the Original Image
+        copy($file, $destinationPath.'/'.$safeNameOrig);
+        copy($file, $destinationPath.'/'.$safeNameLightbox);
         $file->move($destinationPath, $safeName);
 
         // If is not gif file, we will try to reduse the file size
+        // This is for the Lightbox Version.
+        if ($file->getClientOriginalExtension() != 'gif') {
+            // open an image file
+            $imgLb = Image::make($destinationPath.'/'.$safeNameLightbox);
+            // prevent possible upsizing
+            $imgLb->resize(1440, null, function ($constraint) {
+                $constraint->aspectRatio();
+                $constraint->upsize();
+            });
+            // finally we save the image as a new file
+            $imgLb->save();
+        }
+
+        // If is not gif file, we will try to reduse the file size
+        // This is for the Thread Version
         if ($file->getClientOriginalExtension() != 'gif') {
             // open an image file
             $img = Image::make($destinationPath.'/'.$safeName);
             // prevent possible upsizing
-            $img->resize(1440, null, function ($constraint) {
+            $img->resize(783, null, function ($constraint) {
                 $constraint->aspectRatio();
                 $constraint->upsize();
             });
