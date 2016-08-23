fa703ffc49ed7285a765d787f0129d17a761420c
phecho <phecho@phecho.com>
2016-08-23 15:43:18 +0800

    Modify sub months

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -141,7 +141,7 @@ class Thread extends Model implements HasPresenter, TaggableInterface
 
     public function scopePinAndRecentReply($query)
     {
-        return $query->whereRaw("(`created_at` > '".Carbon::today()->subMonth()->toDateString()."' or (`order` > 0) )")
+        return $query->whereRaw("(`created_at` > '".Carbon::today()->subMonths(6)->toDateString()."' or (`order` > 0) )")
                      ->orderBy('order', 'desc')
                      ->orderBy('updated_at', 'desc');
     }
